853c533aa65bdc1563cc92aef1e9ccfe932a8fc1
Alexey <Login-alex200@yandex.ru>
2024-01-22 13:41:27 +0300

    Создание проекта

---
+++ TestApp/Program.cs
@@ -0,0 +1,31 @@
+var app = SetupContainer();
+
+SetupApp();
+
+app.Run();
+
+return;
+
+WebApplication SetupContainer()
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    
+    return builder.Build();
+}
+
+void SetupApp()
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+}
