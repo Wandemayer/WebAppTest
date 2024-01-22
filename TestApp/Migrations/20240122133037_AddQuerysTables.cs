43ccf8e333d145faadb65ec6874f7ecb3798dc3b
Alexey <Login-alex200@yandex.ru>
2024-01-22 16:31:09 +0300

    Миграция: Добавление моделей запросов сбора статистики

---
+++ TestApp/Migrations/20240122133037_AddQuerysTables.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuerysTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Statistic",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    From = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    To = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    VisitsCount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistic", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Queries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DataId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ExecuteStartTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Queries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Queries_Statistic_DataId",
+                        column: x => x.DataId,
+                        principalTable: "Statistic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Queries_DataId",
+                table: "Queries",
+                column: "DataId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Queries");
+
+            migrationBuilder.DropTable(
+                name: "Statistic");
+        }
+    }
+}
