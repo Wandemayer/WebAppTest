8a001fb76e415efa346793182205267c3c4104bc
Alexey <Login-alex200@yandex.ru>
2024-01-22 14:11:25 +0300

    Добавление класса модели данных пользователя

---
+++ TestApp/DB/Model/User.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestApp.DB.Model;
+
+/// <summary>
+/// Модель пользователя.
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Возвращает идентификатор пользователя.
+    /// </summary>
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Возвращает имя пользователя.
+    /// </summary>
+    public string FirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Возвращает Фамилию пользователя.
+    /// </summary>
+    public string LastName { get; set; } = string.Empty;
+}
